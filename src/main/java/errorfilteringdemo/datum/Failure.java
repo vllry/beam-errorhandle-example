61d291fc3e5508295625b6cfe558116fff7d19d8
Vallery Lancey <vallerylancey@gmail.com>
2018-03-14 22:14:43 -0700

    Adjusted Failure constructor.

--- src/main/java/errorfilteringdemo/datum/Failure.java
+++ src/main/java/errorfilteringdemo/datum/Failure.java
@@ -7,19 +7,20 @@ import java.util.Arrays;
 
 public class Failure extends DoFn {
 
+    private String failedClass;
     private String message;
     private String stackTrace;  // May want to expand to an Array/ArrayList depending on use case.
 
     public Failure(Object datum, Throwable thrown) {
+        this.failedClass = datum.getClass().toString();
         this.message = thrown.toString();
         this.stackTrace = Arrays.toString(thrown.getStackTrace());
-
-        System.out.println("FAILURE CAUGHT: " + this.message);
     }
 
     @Override
     public String toString() {
         return "{" +
+                "\nfailedClass: " + this.failedClass +
                 "\nmessage: " + this.message +
                 "\nstackTrace: " + this.stackTrace +
                 "\n}";
